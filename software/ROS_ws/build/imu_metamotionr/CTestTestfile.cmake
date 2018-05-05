5c56f87cf91f014c599dac6eca43f2a0431b2c4a
Andres Tosca Ramirez <atr.andres@gmail.com>
2018-05-04 21:26:46 -0500

    removing ROS generated files

--- software/ROS_ws/build/imu_metamotionr/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/parallels/BoreholeCamera/software/ROS_ws/src/imu_metamotionr
-# Build directory: /home/parallels/BoreholeCamera/software/ROS_ws/build/imu_metamotionr
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- software/ROS_ws/build/rosaria/catkin_generated/rosaria-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(rosaria_MESSAGE_FILES "/home/parallels/BoreholeCamera/software/ROS_ws/src/rosaria/msg/BumperState.msg")
-set(rosaria_SERVICE_FILES "")

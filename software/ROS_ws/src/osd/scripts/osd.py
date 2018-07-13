733c618d6686bf70390357e2d70246ee8b2e9eaf
Andres Tosca <atr.andres@gmail.com>
2018-07-13 09:55:40 -0500

    updating osd code, not finish version

--- software/ROS_ws/src/osd/scripts/osd.py
+++ software/ROS_ws/src/osd/scripts/osd.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 import rospy
 from nav_msgs.msg import Odometry
+import cv2
+import time
+
+#Ip Camera 
+vcap = cv2.VideoCapture("http://192.168.1.77/video.mjpg")
+frame_width = int(vcap.get(3))
+frame_height = int(vcap.get(4))
+
+out = cv2.VideoWriter('/media/pi/HD/logvid/' + time.strftime("%d_%m_%Y_%H_%M") + '.mp4',cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 6, (frame_width,frame_height))
+cont = 0
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %d", data.pose.pose.position.z)

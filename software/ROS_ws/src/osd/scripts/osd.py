dca7912fdf0a46682be7152b43aef42f8bb96539
Andres Tosca <atr.andres@gmail.com>
2018-07-16 20:43:03 -0500

    Adding text to video from ip camera

--- software/ROS_ws/src/osd/scripts/osd.py
+++ software/ROS_ws/src/osd/scripts/osd.py
@@ -3,31 +3,35 @@ import rospy
 from nav_msgs.msg import Odometry
 import cv2
 import time
+meters = 0.0
 
 #Ip Camera 
-vcap = cv2.VideoCapture("http://192.168.1.77/video.mjpg")
+vcap = cv2.VideoCapture("http://192.168.0.101/video.mjpg")
 frame_width = int(vcap.get(3))
 frame_height = int(vcap.get(4))
-
-out = cv2.VideoWriter('/media/pi/HD/logvid/' + time.strftime("%d_%m_%Y_%H_%M") + '.mp4',cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 6, (frame_width,frame_height))
 cont = 0
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %d", data.pose.pose.position.z)
+    global meters 
+    meters = data.pose.pose.position.z
     
 def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
-
+    global meters
+    rospy.init_node('listenerZ', anonymous=True)
     rospy.Subscriber("odom", Odometry, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+       ret, frame = vcap.read()
+       text = "Metros: " + str(meters) 
+       cv2.putText(frame,text,(frame_width/3,frame_height/2),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),1)
+       cv2.imshow('CAM', frame)
+       if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
+       vcap.release()
+       out.release()
+       cv2.destroyAllWindows()
+ 
 
 if __name__ == '__main__':
     listener()

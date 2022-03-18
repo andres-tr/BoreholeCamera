3400deff7294b5c4131f5d91ff2860f0271aebc9
Andres Tosca Ramirez <atr.andres@gmail.com>
2022-03-18 12:48:54 -0600

    Adding timeout for excluding bad data after a connection lost between robot and winch

--- software/ROS_ws/src/imu/scripts/imu_timeout.py
+++ software/ROS_ws/src/imu/scripts/imu_timeout.py
@@ -13,7 +13,7 @@ def callback(data):
     global new_imu_time, old_imu_time
     new_imu_time =  data.header.stamp.secs
     if (new_imu_time - old_imu_time) > 1:
-       rospy.sleep(8)
+       rospy.sleep(9)
     else:      
     	imu_timeout_pub.publish(data)
     old_imu_time = new_imu_time
